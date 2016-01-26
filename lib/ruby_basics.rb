741ae3034cf5d33577ac9b723142e3de9c9677c2
marcelgalang <marcelgalang@gmail.com>
2016-01-26 09:01:49 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,32 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Bob"
+
 end
 
-def argue
-  
+def argue(args)
+args
+
 end
 
-def greeting
-
+def greeting(first, second)
+first
+second
 end
 
 def return_a_value
-
+ return "Nice"
 end
 
 def last_evaluated_value
+  return "expert"
 
 end
 
-def pizza_party
+def pizza_party(x="cheese")
+  return x
 
 end
